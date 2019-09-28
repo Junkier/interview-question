062deb0042817e93df885daefad5a0ea98b1db9a
Jeff.hsuan <logFM3798@gmail.com>
2019-09-28 23:28:36 +0800

    This is for interview

---
+++ front-end/Q2_dataTableList/Q2.js
@@ -0,0 +1,63 @@
+$(function(){
+    // 目標：
+    // 以 Html / Css / JavaScript (jQuery) 作出如 第一回合歌單 類似的表格
+    // 可視為由 後端傳過來的 data ，動態生成到前端網頁顯示
+    
+    // 注意 :
+    // 1. 需使用 JS (jQuery) 操作 DOM 物件，不可更改原始 Q2.html 檔
+    // 2. 可套用已寫好的 class (在 Q2.css)
+    // 3. 程式務必力求 閱讀性 及 維護性
+    var data = [
+        {
+            "singer" : "蘇打綠",
+            "type"   : "獨特樂團",
+            "recent_work" : "下雨的夜晚"
+        },
+        {
+            "singer" : "兄弟本色",
+            "type"   : "饒舌樂團",
+            "recent_work" : "Fly Out"
+        },
+        {
+            "singer" : "李榮浩",
+            "type"   : "作曲家",
+            "recent_work" : "喜劇之王"
+        },
+        {
+            "singer" : "伍佰老師",
+            "type"   : "台客搖滾",
+            "recent_work" : "挪威的森林"
+        },
+        {
+            "singer" : "徐佳瑩",
+            "type"   : "創作精靈",
+            "recent_work" : "莉莉安"
+        },
+        {
+            "singer" : "A-Lin",
+            "type"   : "鐵肺歌姬",
+            "recent_work" : "P.S 我愛你"
+        },
+        {
+            "singer" : "周杰倫",
+            "type"   : "亞洲天王",
+            "recent_work" : "不該"
+        },
+        {
+            "singer" : "張惠妹",
+            "type"   : "亞洲女王",
+            "recent_work" : "姐妹"
+        },
+        {
+            "singer" : "五月天",
+            "type"   : "亞洲天團",
+            "recent_work" : "派對動物"
+        },
+        {
+            "singer" : "5566",
+            "type"   : "經典天團",
+            "recent_work" : "我難過"
+        }
+    ];
+  
+})
